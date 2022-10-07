5da067276eaa5059ce1f61ec15893b3c96f1a375
balem <enriquev.rodriguez@gmail.com>
2022-10-07 14:26:38 -0300

    book services

---
+++ BookStore.API/Models/Book/BookCreateDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using BookStore.API.Models.Author;
+using Newtonsoft.Json;
+
+namespace BookStore.API.Models.Book
+{
+    public class BookCreateDto : BaseDto 
+    {
+        [Required]
+        [StringLength(50)]
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [Required]
+        [JsonProperty("year")]
+        [Range(1000, int.MaxValue)]
+        public string Year { get; set; }
+
+        [Required]
+        [JsonProperty("isbn")]
+        public string Isbn { get; set; }
+
+        [Required]
+        [StringLength(250, MinimumLength =10)]
+        [JsonProperty("summary")]
+        public string Summary { get; set; }
+
+        [JsonProperty("image")]
+        public string Image { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
+    }
+}
+
---
+++ BookStore.API/Models/Book/BookUpdateDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using BookStore.API.Models.Author;
+
+namespace BookStore.API.Models.Book
+{
+    public class BookUpdateDto : BaseDto
+    {
+        [Required]
+        [StringLength(50)]
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [Required]
+        [JsonProperty("year")]
+        [Range(1000, int.MaxValue)]
+        public string Year { get; set; }
+
+        [Required]
+        [JsonProperty("isbn")]
+        public string Isbn { get; set; }
+
+        [Required]
+        [StringLength(250, MinimumLength = 10)]
+        [JsonProperty("summary")]
+        public string Summary { get; set; }
+
+        [JsonProperty("image")]
+        public string Image { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
+    }
+}
+
